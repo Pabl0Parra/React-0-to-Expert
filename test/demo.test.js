4b9bd23288bbf015feebc1b8b87731b696d82b4b
Pabl0Parra <pabsblue16@gmail.com>
2022-11-15 16:18:11 +0100

    demo test with jest

--- test/demo.test.js
+++ test/demo.test.js
@@ -1,5 +1,12 @@
-test("This test can't fail", () => {
-  if (0 === 1) {
-    throw Error("This number cannot be divided it");
-  }
+describe("Test in <DemoComponent />", () => {
+  test("This test can't fail", () => {
+    //1. Initialization
+    const message1 = "Hello world";
+
+    //2. What to do
+    const message2 = message1.trim();
+
+    //3. Watch behaviour
+    expect(message1).toBe(message2);
+  });
 });
