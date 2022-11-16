9bd3e4a43c9ce1147ec1b02bab48d9a2f9d6c0ee
Pabl0Parra <pabsblue16@gmail.com>
2022-11-16 09:57:57 +0100

    installed react/testing-library & created FirstApp component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CounterApp from "./componets/CounterApp";
+// import CounterApp from "./componets/CounterApp";
+import { FirstApp } from "./componets/FirstApp";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CounterApp value={666} />
+    {/* <CounterApp value={666} /> */}
+    <FirstApp />
   </React.StrictMode>
 );
